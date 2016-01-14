c20ce46d50c60178427ef0926a9cc1f46f5dbaf3
Marcus Plutowski <achierius@gmail.com>
2016-01-13 17:13:56 -0800

    Makes the code actually work

--- src/org/usfirst/frc/team115/robot/commands/PIDTester.java
+++ src/org/usfirst/frc/team115/robot/commands/PIDTester.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team115.robot.commands;
 
 import org.usfirst.frc.team115.robot.Robot;
-
+import org.usfirst.frc.team115.robot.subsystems.ShellSubsystem;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 
 public class PIDTester extends PIDCommand{
@@ -9,7 +9,8 @@ public class PIDTester extends PIDCommand{
 	private static final double kP = 0.1;
 	private static final double kI = 0.0;
 	private static final double kD = 0.0;
-
+	private double time;
+	
 	public PIDTester() {
 		super(kP, kI, kD);
 		
@@ -17,39 +18,32 @@ public class PIDTester extends PIDCommand{
 
 	@Override
 	protected double returnPIDInput() {
-		// TODO Auto-generated method stub
 		return Robot.exampleSubsystem.getPosition();
 	}
 
 	@Override
 	protected void usePIDOutput(double output) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-		
+		time = System.currentTimeMillis();
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		
+		ShellSubsystem.writeDT(System.currentTimeMillis()-time);
+		time = System.currentTimeMillis();
 	}
 
 	@Override
 	protected boolean isFinished() {
-		double time = System.currentTimeMillis();
-		System.out.println(time);
 		return false;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
--- src/org/usfirst/frc/team115/robot/subsystems/ShellSubsystem.java
+++ src/org/usfirst/frc/team115/robot/subsystems/ShellSubsystem.java
@@ -1,31 +1,40 @@
 
 package org.usfirst.frc.team115.robot.subsystems;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 import org.usfirst.frc.team115.robot.commands.PIDTester;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * @author Marcus Plutowski
  */
-public class ExampleSubsystem extends Subsystem {
+public class ShellSubsystem extends Subsystem {
     
 	CANTalon tln;
 	
-	public ExampleSubsystem(){
+	static FileWriter file = new FileWriter("C:\\Users\\MVRT\\Desktop\\dT.csv");
+    static PrintWriter print = new PrintWriter(file);
+	public ShellSubsystem()){
 		tln = new CANTalon(6);
 	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	public static void writeDT(double dt){
+		print.print(dt);
+		print.print(",");
+	}
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         setDefaultCommand(new PIDTester());
     }
     
     public void drive(double input){
-    	tln.set(input);
+    	tln.set((input < 1 ? (input > -1 ? input : -1) : 1));
     }
     
     public double getPosition(){
